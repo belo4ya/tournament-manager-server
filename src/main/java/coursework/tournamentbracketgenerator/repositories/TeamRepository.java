33611c3dbdc0e34a1b4812821b59f1fe18e37818
Alexey Kovalev <41exey.kov41ev@gmail.com>
2021-04-19 12:33:56 +0300

    projection для user'а

--- src/main/java/coursework/tournamentbracketgenerator/repositories/TeamRepository.java
+++ src/main/java/coursework/tournamentbracketgenerator/repositories/TeamRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.lang.NonNull;
+import org.springframework.security.access.prepost.PostFilter;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.util.List;
@@ -19,9 +20,10 @@ public interface TeamRepository extends JpaRepository<Team, Long> {
     @RestResource(path = "by_name_i", rel = "by_name_i")
     List<Team> findByNameIgnoreCase(String name);
 
-    @RestResource(path = "users", rel = "users")
-    @PreAuthorize("#username == authentication?.name")
-    List<Team> findByUser_Username(@Param("username") String username);
+    @RestResource(path = "my", rel = "my")
+    @PostFilter("filterObject?.user?.username == authentication?.name")
+    @NonNull
+    List<Team> findAllBy();
 
     @Override
     @PreAuthorize("#team?.user == null or #team?.user?.username == authentication?.name")
--- src/main/java/coursework/tournamentbracketgenerator/repositories/TournamentRepository.java
+++ src/main/java/coursework/tournamentbracketgenerator/repositories/TournamentRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.lang.NonNull;
+import org.springframework.security.access.prepost.PostFilter;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.util.List;
@@ -24,9 +25,10 @@ public interface TournamentRepository extends JpaRepository<Tournament, Long> {
     @Override
     <S extends Tournament> List<S> findAll(Example<S> example);
 
-    @RestResource(path = "by_user", rel = "by_user")
-    @PreAuthorize("#user == authentication?.name")
-    List<Tournament> findByUser_Username(@Param("user") String username);
+    @RestResource(path = "my", rel = "my")
+    @PostFilter("filterObject?.user?.username == authentication?.name")
+    @NonNull
+    List<Tournament> findAllBy();
 
     @Override
     @PreAuthorize("#tournament?.user == null or #tournament?.user?.username == authentication?.name")
