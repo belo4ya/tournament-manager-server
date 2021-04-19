08d2fa88f2a122b8a304d1db81edc4b30cae5f7c
Alexey Kovalev <41exey.kov41ev@gmail.com>
2021-04-19 22:19:13 +0300

    swagger

--- src/main/java/coursework/tournamentbracketgenerator/TournamentBracketGeneratorApplication.java
+++ src/main/java/coursework/tournamentbracketgenerator/TournamentBracketGeneratorApplication.java
@@ -2,8 +2,14 @@ package coursework.tournamentbracketgenerator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
+import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
+@Import({SpringDataRestConfiguration.class, BeanValidatorPluginsConfiguration.class})
 public class TournamentBracketGeneratorApplication {
 
     public static void main(String[] args) {
